f1860adbd3abda28a25b46afa480637d61fd3ea6
bestcoder0612 <bestcoder0612@gmail.com>
2020-12-01 11:14:07 -0800

    feat: update app info

--- src/view/app/info/index.js
+++ src/view/app/info/index.js
@@ -13,7 +13,25 @@ class Info extends Component {
     render() {
         return (
             <Container className="content pl-20 pr-20 mb-30 fade-in">
-                <section className="infoSection">
+                <div className="width-15" style={{textAlign: 'center', paddingLeft: '10px'}}>
+                    <img className="home-image" src="/img/PaqueraMezcal_Landing_LeftArt.png" />
+                </div>
+                <div className="width-70" style={{display: 'flex', justifyContent: 'center', marginTop: '30px'}}>
+                    <p className="infoContent">
+                        <abbr className="f-jeff">Produced</abbr> by a CO-OP in San Miguel Ejutla, Paquera is an award-winning mezcal that was created from a methodology perfected over generations of dedicated family artisans. Our love for Mezcal has only deepened upon being exposed to its unique production process and rich culture.<br /><br />
+                        <abbr className="f-jeff">Certified Artisanal Mezcal / Gluten Free <br />
+                            No Additives / 100% Sustainable <br />
+                            CO-OP Grown & Produced<br /><br />
+                            Available in 3 different expressions: 100% Espadin, 80% Espadin/20% Barril & 100% Barril.<br /><br />
+                            750ml and 200ml bottles available in each expression. <br /><br /></abbr>
+                        Serve neat in a copita or made in cocktail form, Paquera doesn't overwhelm you with smoke but eases you into its smoothness. It can be regarded as “the most approachable premium mezcal in the market”. Balanced and full from the start and finishes buttery. It's to be enjoyed in social settings, either celebratory or with the people you love. <br /><br />
+                        <abbr className="f-jeff">El Espíritu del Mezcal!</abbr>
+                    </p>
+                </div>
+                <div className="width-15" style={{textAlign: 'center', paddingRight: '10px'}}>
+                    <img className="home-image" src="/img/PaqueraMezcal_Landing_RightArt.png" />
+                </div>
+                {/* <section className="infoSection">
                     <div className="infoDiv">
                         <div className="width-25">
                             <img className="width-100" src="/img/PaqueraMezcal_Story_TopLeft3.png" />
@@ -26,7 +44,7 @@ class Info extends Component {
                         </div>
                         <div className="infoContentDiv">
                             <p>
-                                Produced by a CO-OP in San Miguel Ejutla, Paquera is an award-winning mezcal that was created from a methodology perfected over generations of dedicated family artisans. Our love for Mezcal has only deepened upon being exposed to its unique production process and rich culture.<br /><br />
+                                <abbr className="f-jeff">Produced</abbr> by a CO-OP in San Miguel Ejutla, Paquera is an award-winning mezcal that was created from a methodology perfected over generations of dedicated family artisans. Our love for Mezcal has only deepened upon being exposed to its unique production process and rich culture.<br /><br />
                                 <abbr className="f-jeff">Certified Artisanal Mezcal / Gluten Free <br />
                                     No Additives / 100% Sustainable <br />
                                     CO-OP Grown & Produced<br /><br />
@@ -37,7 +55,7 @@ class Info extends Component {
                             </p>
                         </div>
                     </div>
-                </section>
+                </section> */}
             </Container>
         )
     }
