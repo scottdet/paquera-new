6bb35d98c0c832ab45b6595fb1eb94d06decd7f8
bestcoder0612 <bestcoder0612@gmail.com>
2020-12-05 17:57:28 +0800

    feat: updated css styles

--- src/view/age-cookie/index.js
+++ src/view/age-cookie/index.js
@@ -40,12 +40,12 @@ class AgeAllowance extends Component {
             // </div>
             <Container className="full-size m-0 fade-in height-webkit-fill-available">
                 <section className="flex-display" style={{flex: '1'}}>
-                    <div className="flex-space-end-between width-15" style={{flex: '1'}}>
+                    <div className="flex-space-end-between width-15">
                         <img className="full-image" src="/img/PaqueraMezcal_Entry_Ornament1.png" />
                         <img className="full-image" src="/img/PaqueraMezcal_Entry_Ornament2.png" />
                         <img className="full-image" src="/img/PaqueraMezcal_Entry_Ornament6.png" />
                     </div>
-                    <div className="flex-space-end-between width-70" style={{flex: '1'}}>
+                    <div className="flex-space-end-between width-70">
                         <img className="full-image-min-200 mt-20" src="/img/PaqueraMezcal_Entry_Ornament7.png" />
                         <section className="cookie-dialog-section">
                             <img className="width-50 top-left" src="/img/PaqueraMezcal_Story_TopLeft3.png" />
@@ -66,7 +66,7 @@ class AgeAllowance extends Component {
                         </section>
                         <img className="full-image-min-200 mb-20" src="/img/PaqueraMezcal_Entry_Ornament8.png" />
                     </div>
-                    <div className="flex-space-end-between width-15" style={{flex: '1'}}>
+                    <div className="flex-space-end-between width-15">
                         <img className="full-image" src="/img/PaqueraMezcal_Entry_Ornament4.png" />
                         <img className="full-image" src="/img/PaqueraMezcal_Entry_Ornament3.png" />
                         <img className="full-image" src="/img/PaqueraMezcal_Entry_Ornament5.png" />
