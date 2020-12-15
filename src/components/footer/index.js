425575f3965ddd5b636e3222e8474b5783dd460f
bestcoder0612 <bestcoder0612@gmail.com>
2020-12-14 18:18:52 -0800

    feat: updated footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,18 +1,59 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import { Input, Icon } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
+import MailchimpSubscribe from 'react-mailchimp-subscribe'
+
+const CustomForm = ({status, message, onValidated }) => {
+    const [email, setEamil] = useState('');
+    const submit = () => 
+        email &&
+        email.indexOf("@") > -1 &&
+        onValidated({
+            EMAIL: email,
+        });
+    return (
+        <>
+            {status === 'sending' && <div style={{color: "#e1e0d8"}}>sending...</div>}
+            {status === 'error' && (
+                <div style={{color: '#e1e0d8'}} 
+                dangerouslySetInnerHTML={{ __html: message }}></div>
+            )} 
+            {status === "success" && (
+              <div
+                style={{ color: "green" }}
+                dangerouslySetInnerHTML={{ __html: message }}
+              />
+            )}
+            <Input type="email" onChange={e=>setEamil(e.target.value)} placeholder="EMAIL ADDRESS" />
+            <a onClick={submit}><button style={{cursor: 'pointer', top: '3.3px', position: 'relative'}}>SUBMIT</button></a>
+        </>
+    )
+}
 
 class FooterComponent extends Component {
     constructor(props) {
         super(props);
     }
     render() {
+        const url = "https://paqueramezcal.us7.list-manage.com/subscribe/post?u=f4ffa6ffe304739b5053df13a&amp;id=daa6ea6e3e";
         return (
             <section>
                 <section className="footer-desktop">
                     <div className="subscribe">
-                        <Input type="text" placeholder="EMAIL ADDRESS" />
-                        <a href="mailto:sales@paqueralife.com"><button style={{cursor: 'pointer'}}>SUBMIT</button></a>
+                        <MailchimpSubscribe
+                            url={url}
+                            render={({ subscribe, status, message}) => (
+                                <div>
+                                    <CustomForm
+                                        status={status}
+                                        message={message}
+                                        onValidated={formData => subscribe(formData)}
+                                    />
+                                </div>
+                            )}
+                        />
+                        {/* <Input type="text" placeholder="EMAIL ADDRESS" />
+                        <a href="mailto:sales@paqueralife.com"><button style={{cursor: 'pointer'}}>SUBMIT</button></a> */}
                         {/* <p>
                             Subscribe to receive updates, access to exclusive deals, and more.
                         </p> */}
